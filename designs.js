4ad57f8cb5d6b6610fdcfcda5dad2c2c8236f226
Han Brozinski <hannahbrozinski@gmail.com>
2021-06-06 15:48:35 +0200

    Ability to change cell colors by picking a color from color picker

--- designs.js
+++ designs.js
@@ -1,29 +1,29 @@
-// Select color input
-
-
-// Select size input
 let myTable = document.getElementById('pixelCanvas');
 
+let gridSize = document.getElementById('sizePicker');
 
-// When size is submitted by the user, call makeGrid()
-
-const submitGrid = document.getElementById('sizePicker');
-submitGrid.addEventListener('submit', function(event) {
+gridSize.addEventListener('submit', function(event) {
     event.preventDefault();
-    let row = document.getElementById('inputHeight').value;
-    let column = document.getElementById('inputWidth').value; 
-    makeGrid(row, column);
+    // Select size input
+    let height = document.getElementById('inputHeight').value;
+    let width = document.getElementById('inputWidth').value; 
+    // When size is submitted by the user, call makeGrid()
+    makeGrid(height, width);
 });
 
-
-function makeGrid(row, column) {
+// Creates a grid from the user input
+function makeGrid(height, width) {
     myTable.innerHTML = "";
-    for (var i = 0; i < row; i ++) {
-        var makeRow = myTable.insertRow(i);
-        for (var j = 0; j < column; j ++) {
-            var makeCell = makeRow.insertCell(j);
-            
+    for (var i = 0; i < height; i ++) {
+        var row = myTable.insertRow(i);
+        for (var j = 0; j < width; j ++) {
+            var cell = row.insertCell(j);  
+                // Adds Event Listerers to cells
+                cell.addEventListener('click', function(colorChange) {
+                    //console.log(colorChange.target);
+                    var color = document.getElementById('colorPicker').value;
+                    colorChange.target.style.backgroundColor = color;
+            });
         }
-    }
+    };
 };
-
